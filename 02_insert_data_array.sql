78f20fdc03968c34b7bc11e198843c72627b7fd0
Cruz <183213@upslp.edu.mx>
2025-11-23 10:27:22 -0600

    Update 02_insert_data_array.sql

--- 02_insert_data_array.sql
+++ 02_insert_data_array.sql
@@ -1,5 +1,5 @@
 --Insertar al menos 3 productos con diferentes etiquetas
 INSERT INTO productos(nombre,etiquetas)
-VALUES ('Computadora',ARRAY['Tecnlogia','Hogar']),
-       ('Celular',ARRAY['Tecnlogia','Personal']),
+VALUES ('Computadora',ARRAY['tecnología','Hogar']),
+       ('Celular',ARRAY['tecnología','Personal']),
        ('Cama',ARRAY['Hogar','Personal']);
